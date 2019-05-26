22847e3f4054779cd77f2855280d42d061c45c8c
Tyler Brown <tbdatasci@gmail.com>
2019-05-26 12:50:29 -0400

    Upload process_data.py

---
+++ process_data.py
@@ -0,0 +1,105 @@
+# Define imports
+
+import sys
+import pandas as pd
+import numpy as np
+import sqlite3
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    """Loads in data from the messages and categories .csv files"""
+    
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    
+    # merge datasets
+    df = messages.merge(categories, on='id', how='left')
+    
+    return df
+
+
+def clean_data(df):
+    """Refines the loaded data by cleaning it for machine learning use-cases"""
+    
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(pat=';', expand=True)
+    
+    # select the first row of the categories dataframe
+    row = categories[:1]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x: x.str[:-2])
+    
+    # rename the columns of `categories`
+    categories.columns = list(np.array(category_colnames))
+    
+    # Convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1:])
+    
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    # drop the original categories column from `df`
+    df = df.drop('categories', axis=1)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1, sort=False)
+    
+    # Fix column names from concatenation
+    df = df.rename(columns = lambda x : str(x)[:-3] if str(x)[0] == "(" else x)
+    df = df.rename(columns = lambda x : str(x)[2:] if str(x)[0] == "(" else x)
+    
+    # Convert 2's in 'related' to 1's
+    df['related'][df['related'] == 2] = 1
+    
+    # drop duplicates
+    df = df.drop_duplicates()
+    
+    return df
+
+def save_data(df, database_filename):
+    """Save the clean dataset into a sqlite database"""
+    
+    engine_string = 'sqlite:///' + database_filename
+    db_name = database_filename[:-3]
+    
+    engine = create_engine(engine_string)
+    df.to_sql(db_name, engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
